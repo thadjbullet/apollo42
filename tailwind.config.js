565b5690435193f08394163045f622b4184a0b50
Kos <kos.sales@gmail.com>
2022-06-27 23:09:11 +0100

    add postcss and tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+    container: {
+      padding: '0 8rem',
+    },
+  },
+  plugins: [],
+}
